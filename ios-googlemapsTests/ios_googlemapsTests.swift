6ef334337479262482f935f8bab517d7797d9e56
Gaurav Gupta <gaurav@randomsync.net>
2016-11-04 16:40:29 -0700

    Initial Commit

---
+++ ios-googlemapsTests/ios_googlemapsTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_googlemapsTests.swift
+//  ios-googlemapsTests
+//
+//  Created by Gaurav Gupta on 11/4/16.
+//  Copyright © 2016 Gaurav Gupta. All rights reserved.
+//
+
+import XCTest
+@testable import ios_googlemaps
+
+class ios_googlemapsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
